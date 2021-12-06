e43535a105c3e6085e9984914791af5f1deba551
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-06 10:38:29 -0800

    Connected to Firebase

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+import formVisibleReducer from './form-visible-reducer';
+import ticketListReducer from './ticket-list-reducer';
+import { combineReducers } from 'redux';
+
+import { firestoreReducer } from 'redux-firestore';
+
+const rootReducer = combineReducers({
+  formVisibleOnPage: formVisibleReducer,
+  mainTicketList: ticketListReducer,
+  // new line of code below
+  firestore: firestoreReducer
+});
+
+export default rootReducer;
